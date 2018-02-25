538ae8190dc36e750184602632a925d3ba33b8c3
Steven Torres <stevetorres7562@gmail.com>
2018-02-25 23:30:35 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -22,5 +22,9 @@ function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
 }
+do {
+    array = array.slice(1)
+  } while (array.length > 0 && maybeTrue())
 
+  return array
 }
